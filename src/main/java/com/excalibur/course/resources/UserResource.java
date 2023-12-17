2c3cfdeb0938dd121884e0800ab1b0d0bcc4b860
Arthur Queiroz <arthutqueiroz@hotmail.com>
2023-12-16 22:41:55 -0300

    User entity and resource

---
+++ src/main/java/com/excalibur/course/resources/UserResource.java
@@ -0,0 +1,19 @@
+package com.excalibur.course.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.excalibur.course.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+
+    @GetMapping
+    public ResponseEntity<User> findAll(){
+        User u = new User(1L, "Alexia", "alexia@gmail.com", "11110000", "10101");
+        return ResponseEntity.ok().body(u);
+    }
+}
